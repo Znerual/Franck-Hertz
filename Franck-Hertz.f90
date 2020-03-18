fe5d15c9528e93a65aeacdf3bbf3b0e0107453c2
Laurenz <laurenz_ruzicka@gmx.at>
2020-03-18 14:37:23 +0100

    Teilchen können jetzt mehrfach stoßen, Grafik schaut realistischer aus. Exponentialverteilung der Anfangsenergiewerte fehlt noch.

--- Franck-Hertz.f90
+++ Franck-Hertz.f90
@@ -32,18 +32,20 @@
     double precision :: p
     
     ! Variables
-    integer, parameter :: N_trajektorien = 4000 !Anzahl der Trajektorien
+    integer, parameter :: N_trajektorien = 1000, N_simulation = 20 !Anzahl der Trajektorien
     double precision, parameter :: distanz_kg = 2d-2 !Abstand von Kathode zu Gitter
     double precision, parameter :: emfp = 7.1d-2 !emfp (Elastic mean free pathlength) = 1 / n * sigma (n= Dichte, Sigma = Stoßquerschnitt)
     double precision, parameter :: E_anregung = 4.9d0 !eV
-    double precision, parameter :: Ug = 1d0, Ub_max = 2d1, Ub_min = 0.5d0 !Volt
+    double precision, parameter :: Ug = 1d0, Ub_max = 30d1, Ub_min = 0.5d0 !Volt, eV
     double precision, parameter :: U_step = 1d-2
     double precision, parameter :: c = 1.686d5 / distanz_kg
-    double precision :: scaleX, scaleY, x, fx !For creating the rectangle for the monte carlo simulation and the random guesses
-    double precision :: E, Ub , Uh, Ia !Energie des Elektrons (eV), Beschleunigungsspannung, Gegenspannung, Katheodenspannung, Anodenstrom
-    integer :: i,n=0, t
-    double precision :: dummy, max = 0
-    
+    double precision, parameter :: simulations_breite = distanz_kg / N_simulation
+    double precision, parameter :: W = 4d0, Temp = 1.2d3, kB = 1.38064852d-23, Uh = 1d3 !Zur bestimmung der Exponentialverteilung der Startenergie
+    double precision :: scaleX, scaleY, x,delta_x, fx !For creating the rectangle for the monte carlo simulation and the random guesses
+    double precision :: E, Ub , Ia !Energie des Elektrons (eV), Beschleunigungsspannung, Gegenspannung, Katheodenspannung, Anodenstrom
+    integer :: i,n=0,j, t
+    double precision :: dummy
+    double precision :: te !Temperatur, welche zufallsverteilt wird
    
     
     scaleX = distanz_kg
@@ -58,11 +60,18 @@
     
     do Ub = Ub_min, Ub_max, U_step
         do i = 0, N_trajektorien
-            x = rand() * scaleX
-            fx = rand() * scaleY
-            E = Ub/distanz_kg * x
-            if (fx > p(x, emfp) .and. E > E_anregung) E = E - E_anregung !Freie Weglänge ist zu klein, genug Energie zur Anregung vorhanden, Teilchen stößt
-            E = E + (scaleX - x) * Ub/distanz_kg - Ug!Energie (nach Stoß) weiter erhöht und durch Gegenspannung erniedrigt
+            
+            te = rand() * 100d0 + temp
+            E = exp(- Uh / (kB * te))
+            !E = 0
+            do j = 0,N_simulation
+                delta_x = rand() * simulations_breite
+                x = delta_x + j * simulations_breite 
+                fx = rand() * scaleY
+                E = E + Ub/distanz_kg * delta_x
+                if (fx > p(x, emfp) .and. E > E_anregung) E = E - E_anregung !Freie Weglänge ist zu klein, genug Energie zur Anregung vorhanden, Teilchen stößt
+            end do
+            E = E - Ug
             if (E >= 0) n = n + 1!Teilchen ist an der Anode angekommen
             !print*, "Teilchen simuliert bei x: " , x ," f(x): ", fx, " mit Energie " , (Ub-Ug), " hat jetzt Energie ", E
         end do
